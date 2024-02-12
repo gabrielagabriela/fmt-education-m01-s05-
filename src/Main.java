a874b2f6b1b3db1728c01cbdcd19403befe85c9c
gabrielagabriela <gabrielaa.slv@gmail.com>
2024-02-12 15:31:54 -0300

    exercicio 03

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Main {
     public static void main(String[] args) {
 
 
+        List<Jogador> melhoresJogadores = new ArrayList<>();
     }
 }
