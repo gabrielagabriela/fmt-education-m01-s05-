621b0bfa449e8c3e1a4304c00840dc533ffd88e5
gabrielagabriela <gabrielaa.slv@gmail.com>
2024-02-12 13:49:47 -0300

    exercicio 01

---
+++ src/Jogador.java
@@ -0,0 +1,11 @@
+public class Jogador {
+
+    public String nome;
+    public int idade;
+    public int pontuacao;
+    public int numeroTentativas;
+
+    public void adicionaPontos(){}
+    public void perdePontos(){}
+    public void adicionaTentativa(){}
+}
